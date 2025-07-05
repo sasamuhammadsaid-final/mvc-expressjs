56b2ba58e82be1e8f427d94f83d841ee2af46cd9
Sasa Muhammad Said <sasamuhammadsaid.final@gmail.com>
2025-07-05 11:40:23 +0700

    versi blocking code

--- models/product.js
+++ models/product.js
@@ -1,15 +1,32 @@
-const products = [];
+const fs = require("fs").promises;
+const rootDir = require('../util/path');
+const path = require('path');
+const filePath = path.join(rootDir, 'data', 'products.json');
+
+async function readJSONFile() {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(`Error reading file from disk: ${err}`);
+    return [];
+  }
+}
 
 module.exports = class Product {
   constructor(t) {
-    this. title = t;
+    this.title = t;
   }
 
-  save() {
-    products.push(this)
+  async save() {
+     const products = await readJSONFile();
+     products.push(this);
+     await fs.writeFile(filePath, JSON.stringify(products, null, 2));
   }
 
-  static fetchAll() {
-    return products;
+  static async fetchAll(cb) {
+    const products = await readJSONFile();
+    cb(products);
   }
+  
 }
